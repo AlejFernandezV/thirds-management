92656f7ee82a05a4deed9ab42d60239a9729a861
AlejFernandezV <alejfernandez@unicauca.edu.co>
2024-03-05 22:47:46 -0500

    first commit

---
+++ src/main/java/com/thirdsmanagement/ThirdsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.thirdsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdsManagementApplication.class, args);
+	}
+
+}
